9894dc40bb5f9375488ca9ce06322ed06f147fa7
MOHAMEDGADO21 <arabianights200001@gmail.com>
2024-08-23 06:40:05 -0700

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - printa program name, followed by a new lin e
+ * argc: int
+ * argv: list
+ * Return: 0
+ */
+
+int main(int argc, charr const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return(0);	
+}
